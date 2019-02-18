0c97d8cba716c6e06aad8ae6ed8d718eb01debf5
Matt Seeley <mattseeley@hotmail.com>
2019-02-17 18:12:14 -1000

    build: remove package workspace spec template
    The spec template was useful initially to allow Jest to pass without any sub-package tests. Although that need is no longer applicable.

--- scripts/workspace/template/lib/index.spec.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env jest */
-const helloWorld = require("./index");
-
-describe("Hello world", () => {
-  test("Returns expected response", () => {
-    expect(helloWorld()).toEqual("Hello world");
-  });
-});
